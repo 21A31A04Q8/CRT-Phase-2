18000d8ca742e48fa8670ac74631ea245685fed3
21A31A04Q8 <146418306+21A31A04Q8@users.noreply.github.com>
2024-05-29 13:35:58 +0530

    Add files via upload

---
+++ binary search.py
@@ -0,0 +1,21 @@
+nums=list(map(int,input().split()))
+target=int(input())
+nums=sorted(nums)
+
+left=0
+right=len(nums)-1
+flag=-1
+
+while left<= right:
+    mid=(left + right)//2
+    if nums[mid]==target:
+        flag=mid
+        break
+    elif nums[mid]>target:
+        right=mid-1 
+    else:
+        left=mid+1
+if flag==-1:
+    print("target not found")
+else:
+    print("target found at index:",flag)
